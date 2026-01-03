7f1d57c885c042d0a23cb6b42bdd0904a7cabaeb
BOSSLADY <gennymiriane06@gmail,com>
2026-01-03 02:37:36 +0100

    [bash-todo-app] i corrected errors found in  todo.sh

--- todo.sh
+++ todo.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
-FILE="tasks.txt"
+del)
+    if [ -z "$2" ]; then
+        echo "Error: Please provide a task number."
+        exit 1
+    fi
 
-if [ ! -f "$FILE" ]; then
-    touch "$FILE"
-fi
+    total_tasks=$(wc -l < "$FILE")
 
-if [ "$1" = "add" ]; then
-    echo "$2" >> "$FILE"
-    echo "Task added."
-fi
+    if [ "$2" -lt 1 ] || [ "$2" -gt "$total_tasks" ]; then
+        echo "Error: Task number does not exist."
+        exit 1
+    fi
 
-if [ "$1" = "list" ]; then
+    > new.txt
     number=1
-    while read -r line; do
-        echo "$number. $line"
-        number=$((number+1))
-    done < "$FILE"
-fi
 
-if [ "$1" = "del" ]; then
-    number=1
-    
     while read -r line; do
-        if [ "$number" != "$2" ]; then
+        if [ "$number" -ne "$2" ]; then
             echo "$line" >> new.txt
         fi
-        number=$((number+1))
+        ((number++))
     done < "$FILE"
 
     mv new.txt "$FILE"
-    echo "Task deleted."
-fi
+    echo "Task deleted successfully."
+    ;;
